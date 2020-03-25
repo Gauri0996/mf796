d8ee89750ac37f790923439963f11be2a4ac7fc8
gkepertis <gkepertis@gmail.com>
2020-03-25 15:17:26 -0400

    First pieces of code
    Appendix A

--- code/shrinkage.py
+++ code/shrinkage.py
@@ -4,3 +4,31 @@ can do all the operations described in appendices A and B of the paper.
 The goal is to have it output our new matrix which we will use to optimize our portfolio weights.
 To do that, we need to find the shrinkage intensity, which is the most involved part of the calculation
 """
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+stock_prices = pd.read_csv(r"C:\Users\jojis\OneDrive\Documents\GitHub\mf796\data\adjclosepx.csv", index_col="Date")
+stock_prices.dropna(inplace=True)
+returns = stock_prices.pct_change(1)
+returns = returns.loc['04/01/2016':'31/12/2019',:]
+covariance = returns.cov()
+correlation = returns.corr()
+
+class Covariance_Shrinkage():
+
+    def __init__(self,cov_mat, corr_mat):
+        self.cov_mat = np.array(cov_mat)
+        self.corr_mat = np.array(corr_mat)
+
+    def get_F(self):
+        N = len(self.corr_mat[0:]) 
+        F = np.zeros(  (N,N)   )
+        r_bar_sum = 0 
+        for i in range(0,N-1):
+            for j in range(i+1,N):
+                r_bar_sum += corr_mat[i,j]
+        r_bar = 2/(N*(N-1))*r_bar_sum
+        return r_bar
+
