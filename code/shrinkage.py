d4b943b4f873acba4f57daa51ef54028b771520f
gkepertis <gkepertis@gmail.com>
2020-03-25 13:58:18 -0400

    Create shrinkage.py

---
+++ code/shrinkage.py
@@ -0,0 +1,6 @@
+"""
+Let's create a class that takes in the sample covariance matrix as an input and 
+can do all the operations described in appendices A and B of the paper.
+The goal is to have it output our new matrix which we will use to optimize our portfolio weights.
+To do that, we need to find the shrinkage intensity, which is the most involved part of the calculation
+"""
