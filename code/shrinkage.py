60cef3aea3e79ededfda0b9b55fc2fd3e97cf7d6
gkepertis <gkepertis@gmail.com>
2020-03-25 15:43:00 -0400

    Starting appendix B
    Added a method for calculating gamma

--- code/shrinkage.py
+++ code/shrinkage.py
@@ -9,6 +9,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+""" Change the path to your path if you are gonna run this """
 stock_prices = pd.read_csv(r"C:\Users\jojis\OneDrive\Documents\GitHub\mf796\data\adjclosepx.csv", index_col="Date")
 stock_prices.dropna(inplace=True)
 returns = stock_prices.pct_change(1)
@@ -21,27 +22,39 @@ class Covariance_Shrinkage():
     def __init__(self,cov_mat, corr_mat):
         self.cov_mat = np.array(cov_mat)
         self.corr_mat = np.array(corr_mat)
+        self.N = len(corr_mat[0:])
 
     def get_F(self):
-        N = len(self.corr_mat[0:]) 
-        F = np.zeros(  (N,N)   )
+
+        F = np.zeros(  (self.N,self.N)   )
         r_bar_sum = 0 
 
-        for i in range(0,N-1):
-            for j in range(i+1,N):
+        for i in range(0,self.N-1):
+            for j in range(i+1,self.N):
                 r_bar_sum += self.corr_mat[i,j]
-        r_bar = (2/(N*(N-1)))*r_bar_sum
+        r_bar = (2/(self.N*(self.N-1)))*r_bar_sum
         
-        for i in range(0,N):
-            for j in range(0,N):
+        for i in range(0,self.N):
+            for j in range(0,self.N):
                 if i == j:
                     F[i,j] = self.cov_mat[i,j]
                 else:
                     F[i,j] = r_bar * np.sqrt(self.cov_mat[i,i]*self.cov_mat[j,j])
         return F
+    
+    def get_gamma(self):
+        gamma = 0
+        F = self.get_F()
+        for i in range(0,self.N):
+            for j in range(0,self.N):
+                gamma += ( (F[i,j] - self.cov_mat[i,j])**2 )
+        return gamma
 
+#making sure things are working
 test = Covariance_Shrinkage(covariance,correlation)
 F = test.get_F()
+gamma = test.get_gamma()
 print(F)
+print(gamma)
 
 
