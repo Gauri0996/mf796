b6be9c342327d35258d67f533b7a22d11dc46856
rjcook <rjcook@bu.edu>
2020-03-25 21:00:17 -0400

    started working on get_delta function. it's incomplete and currently takes 45 minutes to run :(

--- code/shrinkage.py
+++ code/shrinkage.py
@@ -9,7 +9,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-stock_prices = pd.read_csv(r"C:\Users\jojis\OneDrive\Documents\GitHub\mf796\data\adjclosepx.csv", index_col="Date")
+george = r"C:\Users\jojis\OneDrive\Documents\GitHub\mf796\data\adjclosepx.csv"
+robbie = "/home/robbie/Documents/MSMF/Spring2020/MF796/project/data/adjclosepx.csv"
+stock_prices = pd.read_csv(robbie, index_col="Date")
 stock_prices.dropna(inplace=True)
 returns = stock_prices.pct_change(1)
 returns = returns.loc['04/01/2016':'31/12/2019',:]
@@ -40,8 +42,34 @@ class Covariance_Shrinkage():
                     F[i,j] = r_bar * np.sqrt(self.cov_mat[i,i]*self.cov_mat[j,j])
         return F
 
+    def get_delta(self):
+        """
+        returns an estimate of the optimal shrinkage constant (intensity)
+        see appendix B in honey.pdf
+        """
+
+        T = len(returns)
+        N = len(returns.columns)
+        pi_hat = 0
+        rho_hat = 0
+        gamma_hat = 1
+        #pi_hat - this takes way too long to run
+        for i in range(N-1):
+            for j in range(N-1):
+                for t in range(T-1):
+                    pi_hat += ((returns.iloc[t:t+1,j:j+1].values[0][0] - returns.loc[: , returns.columns[j]].mean())*(returns.iloc[t:t+1,i:i+1].values[0][0] - returns.loc[: , returns.columns[i]].mean()) - self.cov_mat[i,j])**2
+                    #print statements to make sure something is actually happening
+                    print("i =", i)
+                    print("j =", j)
+                    print("pi_hat =", pi_hat)
+                pi_hat *= 1/T
+        #rho_hat and gamma_hat - to be completed
+
+        kappa_hat = (pi_hat-rho_hat)/gamma_hat
+        delta_hat = max(0,min((kappa_hat/T),1))
+        return delta_hat
+
 test = Covariance_Shrinkage(covariance,correlation)
 F = test.get_F()
 print(F)
-
-
+test.get_delta()
