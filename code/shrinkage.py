267ad8bc5bb6c7249e6612196eb52eeb0392f77b
gkepertis <gkepertis@gmail.com>
2020-03-29 13:21:50 -0400

    pi updates

--- code/shrinkage.py
+++ code/shrinkage.py
@@ -12,19 +12,22 @@ import matplotlib.pyplot as plt
 """ Change the path to your path if you are gonna run this """
 george = r"C:\Users\jojis\OneDrive\Documents\GitHub\mf796\data\adjclosepx.csv"
 robbie = "/home/robbie/Documents/MSMF/Spring2020/MF796/project/data/adjclosepx.csv"
-stock_prices = pd.read_csv(robbie, index_col="Date")
+stock_prices = pd.read_csv(george, index_col="Date")
 stock_prices.dropna(inplace=True)
-returns = stock_prices.pct_change(1)
-returns = returns.loc['04/01/2016':'31/12/2019',:]
-covariance = returns.cov()
-correlation = returns.corr()
+stock_returns = stock_prices.pct_change(1)
+stock_returns = stock_returns.loc['02/01/2018':'31/12/2019',:]
+covariance = stock_returns.cov()
+correlation = stock_returns.corr()
 
 class Covariance_Shrinkage():
 
-    def __init__(self,cov_mat, corr_mat):
+    def __init__(self,cov_mat, corr_mat, ret):
         self.cov_mat = np.array(cov_mat)
         self.corr_mat = np.array(corr_mat)
         self.N = len(corr_mat[0:])
+        self.ret = ret
+        self.T = len(ret.iloc[:,0])
+        self.mean_ret = ret.mean()
 
     def get_F(self):
 
@@ -52,29 +55,38 @@ class Covariance_Shrinkage():
                 gamma += ( (F[i,j] - self.cov_mat[i,j])**2 )
         return gamma
 
+    # def get_pi(self):
+    #     #this takes way too long to run
+    #     # ^^^ passing the returns either as an argument to this function or in the init might be a good idea (that way T can also be called with self)
+    #     pi = 0
+    #     T = len(returns)
+    #     #in all the loops below, I think we need to remove the -1. The range function is not inclusive for the last number (i.e. range(3) = [0,1,2])
+    #     for i in range(0,self.N-1):
+    #         for j in range(0,self.N-1):
+    #             for t in range(0,T-1):
+    #                 pi += ((returns.iloc[t:t+1,i:i+1].values[0][0] - returns.loc[: , returns.columns[i]].mean())*(returns.iloc[t:t+1,j:j+1].values[0][0] - returns.loc[: , returns.columns[j]].mean()) - self.cov_mat[i,j])**2
+    #                 #print statements to make sure something is actually happening
+    #                 #print("i =", i)
+    #                 #print("j =", j)
+    #                 #print("pi =", pi)
+    #             pi *= 1/T
+    #     return pi
     def get_pi(self):
-        #this takes way too long to run
-        # ^^^ passing the returns either as an argument to this function or in the init might be a good idea (that way T can also be called with self)
         pi = 0
-        T = len(returns)
-        #in all the loops below, I think we need to remove the -1. The range function is not inclusive for the last number (i.e. range(3) = [0,1,2])
-        for i in range(0,self.N-1):
-            for j in range(0,self.N-1):
-                for t in range(0,T-1):
-                    pi += ((returns.iloc[t:t+1,i:i+1].values[0][0] - returns.loc[: , returns.columns[i]].mean())*(returns.iloc[t:t+1,j:j+1].values[0][0] - returns.loc[: , returns.columns[j]].mean()) - self.cov_mat[i,j])**2
-                    #print statements to make sure something is actually happening
-                    #print("i =", i)
-                    #print("j =", j)
-                    #print("pi =", pi)
-                pi *= 1/T
+        for i in range(0,self.N):
+            for j in range(0,self.N):
+                for t in range(0,self.T):
+                    pi += ( (self.ret.iloc[t,i] - self.mean_ret[i])*(self.ret.iloc[t,j] - self.mean_ret[j]) - self.cov_mat[i,j] )**2
+        pi  = pi * (1/self.T)
         return pi
 
+
 #making sure things are working
-test = Covariance_Shrinkage(covariance,correlation)
+test = Covariance_Shrinkage(covariance,correlation, stock_returns)
 F = test.get_F()
 gamma = test.get_gamma()
-#pi = test.get_pi()
+pi = test.get_pi()
 print(F)
 print(gamma)
-#print(pi)
+print(pi)
 
